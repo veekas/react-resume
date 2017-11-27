67ba957da42d2093b1f8b15d676397f2733f5bc5
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2017-11-27 11:51:02 -0500

    chore: edit port to listen

--- dev-server.js
+++ dev-server.js
@@ -25,7 +25,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, 'localhost', (err) => {
+app.listen(PORT, '0.0.0.0', (err) => {
   if (err) {
     console.log(err);
     return;
