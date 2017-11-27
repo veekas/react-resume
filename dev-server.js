592716f1e40a58e4968e7531e9350e81c4ea3fa9
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2017-11-27 12:23:02 -0500

    chore: troubleshoot server 3

--- dev-server.js
+++ dev-server.js
@@ -2,43 +2,30 @@
 const path = require('path');
 const express = require('express');
 const webpack = require('webpack');
-const morgan = require('morgan');
-const compression = require('compression');
-
-const PORT = process.env.PORT || 3213;
 const config = require('./webpack.config.dev');
 
 const app = express();
 const compiler = webpack(config);
 
-app.use(morgan('dev'));
-app.use(compression());
+const PORT = process.env.PORT || 3213;
 
-// app.use(require('webpack-dev-middleware')(compiler, {
-//   noInfo: true,
-//   publicPath: config.output.publicPath,
-//   stats: { colors: true },
-// }));
+app.use(express.static(__dirname + 'index.html'));
 
-// app.use(require('webpack-hot-middleware')(compiler));
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath,
+  stats: { colors: true },
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
 
 app.get('/favicon.ico', (req, res) => {
   res.send(path.join(__dirname + 'favicon.ico'));
 });
 
-app.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
-
-app.use((err, req, res) => {
-  console.error(err);
-  console.error(err.stack);
-  res.status(err.status || 500).send(err.message || 'Internal server error.');
-});
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'index.html'));
-// });
 
 app.listen(PORT, (err) => {
   if (err) {
