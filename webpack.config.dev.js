91de98f03f536f0811eb65e5886b657920312e29
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2018-02-23 16:51:32 -0500

    add svg loaders and svg logo file

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -26,6 +26,9 @@ module.exports = {
     {
       test: /\.css$/,
       loader: 'style-loader!css-loader',
+    }, {
+      test: /\.svg$/,
+      loader: 'svg-url-loader?pngScale=2',
     }],
   },
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -32,6 +32,9 @@ module.exports = {
     }, {
       test: /\.css$/,
       loader: 'style-loader!css-loader',
+    }, {
+      test: /\.svg$/,
+      loader: 'svg-url-loader?pngScale=2',
     }],
   },
 };
